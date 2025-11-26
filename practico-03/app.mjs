3d18776bebc0e8933a834519ad0604b89aa430aa
Emiliano-Herrera <emilianoherrera701@gmail.com>
2025-11-26 15:40:59 -0300

    base del sprint4.

---
+++ practico-03/app.mjs
@@ -0,0 +1,31 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { connectDB } from './config/dbConfig.mjs';
+import superHeroRoutes from './routes/superHeroRoutes.mjs';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.json());
+
+connectDB();
+// configuracion del motor de vistar ejs
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+// esto es para archivos estaticos (css, js)...
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/api', superHeroRoutes); // ← PARA API (/api/heroes, etc.)
+// El manejo de rutas no encontradas
+app.use((req, res) => {
+    res.status(404).send({ mensaje: 'Ruta no encontrada' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
---
+++ practico-03/routes/superHeroRoutes.mjs
@@ -0,0 +1,54 @@
+import express from 'express';
+
+import { 
+    ObtenerSuperHeroePorIdController,
+    buscarSuperHeroesPorAtributoController, 
+    obtenerSuperHeroesMayoresDe30Controller, 
+    crearSuperHeroeController, 
+    actualizarSuperHeroeController, 
+    eliminarSuperPorIdController, 
+    eliminarSuperPorNombreController, 
+    mostrarFormularioAgregarController, 
+    mostrarFormularioEditarController,
+    mostrarMenu
+    
+} from '../controllers/superheroesController.mjs';
+
+import { 
+    validarCrearSuperHeroe, 
+    validarActualizarSuperHeroe, 
+    validarId, 
+    validarNombre 
+} from '../validation/validationRules.js';
+import { handleValidationErrors } from '../validation/errorMiddleware.js';
+const router = express.Router();
+
+// ====================
+// RUTAS DE VISTAS
+// ====================
+// Formulario para agregar superheroes
+router.get('/heroes/nuevo', mostrarFormularioAgregarController);
+// Formulario para editar superheroes
+router.get('/heroes/editar/:id', mostrarFormularioEditarController);
+// Mostrar todos los super en la vista principal...
+router.get('/menu', mostrarMenu);
+
+
+// ====================
+// RUTAS DE API
+// ====================
+router.get('/heroes/:id', ObtenerSuperHeroePorIdController);
+router.get('/heroes/buscar/:atributo/:valor', buscarSuperHeroesPorAtributoController);
+router.get('/heroes/mayor/mayores-30', obtenerSuperHeroesMayoresDe30Controller);
+
+// Rutas POST
+router.post('/crearSuperHeroe', validarCrearSuperHeroe, handleValidationErrors, crearSuperHeroeController);
+
+// Rutas PUT
+router.put('/actualizarHeroe/:id', validarActualizarSuperHeroe, handleValidationErrors, actualizarSuperHeroeController);
+
+// Rutas DELETE
+router.delete('/eliminarSuperID/:id', validarId, handleValidationErrors, eliminarSuperPorIdController);
+router.delete('/eliminarSuperNombre/:nombreSuperHeroe', validarNombre, handleValidationErrors, eliminarSuperPorNombreController);
+
+export default router;
